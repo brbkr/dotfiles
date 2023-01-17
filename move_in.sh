5fbd3da3e57791d79eb972b971d771589408409a
Jeff Brubaker <jeff.brubaker@gmail.com>
2023-01-17 11:17:16 -0500

    Reconfigure for GNU Stow

--- move_in.sh
+++ move_in.sh
@@ -8,30 +8,20 @@ rm -f	~/.cshrc \
 	~/.profile \
 	~/.Xdefaults
 
-if [ -d ~/.dotfiles ]; then
-	cd ~/.dotfiles
+if [ -d ~/dotfiles ]; then
+	cd ~/dotfiles
 	git pull --ff-only
 else
-	git clone git@github.com:brbkr/dotfiles ~/.dotfiles
+	git clone git@github.com:brbkr/dotfiles ~/dotfiles
 fi
 
 [ -d ~/.git ] || mkdir ~/.git
 [ -d ~/.ssh ] || mkdir ~/.ssh
 [ -d ~/.vim ] || mkdir ~/.vim
 
-cd ~/.dotfiles
-for i in .???*; do
-	[ "$i" = ".git" ] && continue
-	[ ! -f "$i" ] && continue
-	rm -f ~/$i
-	ln -s .dotfiles/$i ~/$i
+cd ~/dotfiles
+for i in *; do
+    [ -d $i ] && stow $i
 done
-
-(cd ~/.dotfiles/.config; tar c .) | (cd ~/.config; tar xv -)
-
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-	git clone https://github.com/VundleVim/Vundle.vim.git \
-		~/.vim/bundle/Vundle.vim
-fi
 
 echo "vim +PluginInstall +qall"
