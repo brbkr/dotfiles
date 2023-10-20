e8146778c0e600e54c4708c2eea9d5d07f891dd2
Jeff Brubaker <jeff.brubaker@gmail.com>
2023-10-20 07:14:32 -0700

    Disable color scheme, which was affecting startup performance

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,36 +1,38 @@
-vim.opt.termguicolors = true
+-- Disabled: found that this was affecting startup performance
 
--- vim.g.everforest_background = 'medium'
--- vim.g.everforest_better_performance = 1
--- vim.g.everforest_enable_italic = 0
--- vim.g.everforest_disable_italic_comment = 1
--- vim.g.everforest_transparent_background = 1
-
--- require('rose-pine').setup({ disable_italics = true, })
--- vim.cmd.colorscheme("base16-grayscale-dark")
-vim.cmd.colorscheme("base16-default-dark")
-vim.cmd.colorscheme("mellifluous")
--- vim.cmd.colorscheme("kanagawa-dragon")
--- vim.cmd.colorscheme("everforest")
-
--- vim.cmd("hi clear")
--- vim.g.colors_name = 'base16-default-dark-jtb'
--- require('base16-colorscheme').setup({
---     base00 = '#404040', base01 = '#606060', base02 = '#d0d0d0', base03 = '#707070',
---     base04 = '#b8b8b8', base05 = '#d8d8d8', base06 = '#e8e8e8', base07 = '#f8f8f8',
---     base08 = '#ab4642', base09 = '#dc9656', base0A = '#f7ca88', base0B = '#a1b56c',
---     base0C = '#86c1b9', base0D = '#7cafc2', base0E = '#ba8baf', base0F = '#a16946'
---     })
-
--- Disable background (some colorschemes use this)
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" });
-vim.api.nvim_set_hl(0, "StatusLine", { bg = "#404040" });
-vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#282828" });
-vim.api.nvim_set_hl(0, "Comment", { fg = "#707070" });
-vim.api.nvim_set_hl(0, "TSComment", { fg = "#707070" });
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
-
--- Another option is to just use the terminal
 -- vim.opt.termguicolors = true
--- vim.cmd.colorscheme("default")
+
+-- -- vim.g.everforest_background = 'medium'
+-- -- vim.g.everforest_better_performance = 1
+-- -- vim.g.everforest_enable_italic = 0
+-- -- vim.g.everforest_disable_italic_comment = 1
+-- -- vim.g.everforest_transparent_background = 1
+
+-- -- require('rose-pine').setup({ disable_italics = true, })
+-- -- vim.cmd.colorscheme("base16-grayscale-dark")
+-- vim.cmd.colorscheme("base16-default-dark")
+-- vim.cmd.colorscheme("mellifluous")
+-- -- vim.cmd.colorscheme("kanagawa-dragon")
+-- -- vim.cmd.colorscheme("everforest")
+
+-- -- vim.cmd("hi clear")
+-- -- vim.g.colors_name = 'base16-default-dark-jtb'
+-- -- require('base16-colorscheme').setup({
+-- --     base00 = '#404040', base01 = '#606060', base02 = '#d0d0d0', base03 = '#707070',
+-- --     base04 = '#b8b8b8', base05 = '#d8d8d8', base06 = '#e8e8e8', base07 = '#f8f8f8',
+-- --     base08 = '#ab4642', base09 = '#dc9656', base0A = '#f7ca88', base0B = '#a1b56c',
+-- --     base0C = '#86c1b9', base0D = '#7cafc2', base0E = '#ba8baf', base0F = '#a16946'
+-- --     })
+
+-- -- Disable background (some colorschemes use this)
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" });
+-- vim.api.nvim_set_hl(0, "StatusLine", { bg = "#404040" });
+-- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#282828" });
+-- vim.api.nvim_set_hl(0, "Comment", { fg = "#707070" });
+-- vim.api.nvim_set_hl(0, "TSComment", { fg = "#707070" });
+-- -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
+
+-- -- Another option is to just use the terminal
+-- -- vim.opt.termguicolors = true
+-- -- vim.cmd.colorscheme("default")
