5c4faf31338c5a5941a820373ef1bdfb40e63099
Jeff Brubaker <jeff.brubaker@gmail.com>
2023-10-21 19:08:32 -0400

    jtb.colors DNE; maybe not added

--- nvim/.config/nvim/lua/jtb/init.lua
+++ nvim/.config/nvim/lua/jtb/init.lua
@@ -1,4 +1,4 @@
 require("jtb.remap") -- Make sure to set `mapleader` before lazy so your mappings are correct
 require("jtb.set")
 require("jtb.lazy")
-require("jtb.colors")
+-- require("jtb.colors")
