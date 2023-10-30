8619f3a893780521a4eed9d18e9546426b7f066e
Jeff Brubaker <jeff.brubaker@gmail.com>
2023-10-30 16:32:47 -0700

    Add Perforce autocommands

--- nvim/.config/nvim/lua/jtb/filetype.lua
+++ nvim/.config/nvim/lua/jtb/filetype.lua
@@ -5,3 +5,19 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	vim.o.softtabstop = 2;
     end
 })
+
+-- Perforce commit messages
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    pattern = '/tmp/tmp.[0-9]*.[0-9]*.[0-9]*',
+    callback = function(ev)
+	vim.o.filetype = 'perforce';
+    end
+})
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = "perforce",
+    callback = function(ev)
+	vim.o.shiftwidth = 8;
+	vim.o.softtabstop = 0;
+	vim.o.expandtab = false;
+    end
+})
