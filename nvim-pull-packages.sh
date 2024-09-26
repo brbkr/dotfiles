197ed30c97cfc7dfb65b110e1f7040bd0fd28c3b
Jeff Brubaker <jeff.brubaker@gmail.com>
2024-09-26 09:01:06 -0700

    Add nvim-vp4

--- nvim-pull-packages.sh
+++ nvim-pull-packages.sh
@@ -15,3 +15,4 @@ function pullpackage()
 set -x
 pullpackage nvim-lspconfig https://github.com/neovim/nvim-lspconfig
 pullpackage base16-nvim https://github.com/RRethy/base16-nvim
+pullpackage vim-vp4 https://github.com/ngemily/vim-vp4
